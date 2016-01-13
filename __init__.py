d37e7a86baa72746357d343eb021ade786cfa86c
tungpun <htung.nht@gmail.com>
2016-01-13 22:30:32 +0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,27 @@
+import pwn
+from termcolor import colored
+
+class PunPwn():
+    def __init__(self, host, port):
+        self.conn = pwn.remote(host, port)
+
+
+    def sent(self, s):
+        self.conn.sendline(s)
+        print colored('[+] Sent: ' + s, 'yellow')
+
+
+    def recvall(self):
+        print colored(self.conn.recvall(), 'green', 'on_grey')
+        
+    
+    def recvuntil(self, s):
+        print colored(self.conn.recvuntil(s), 'green', 'on_grey')
+
+
+    def interactive(self):
+        self.conn.interactive()
+
+
+if __name__ == '__main__':
+    print "Hello !"
